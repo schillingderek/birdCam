d72ee74baad245de1ce7eb585d59494a1c642aae
schillingderek <schilling.derek@gmail.com>
2024-06-06 11:51:57 -0500

    removing note

--- motion_camera.py
+++ motion_camera.py
@@ -108,7 +108,6 @@ def livedetection():
             error = mse(frame1gray,frame2gray)
             print(error)
             pir_motion_sensor = GPIO.input(PIR_PIN)
-            # require camera and pir sensor
             print("motiondetection", motiondetection, "motionvideostart", motionvideostart, "cameramotiondetected", str(error>=20.0), "pirmotionsensor", pir_motion_sensor)
             if error>=20.0 and pir_motion_sensor:
                 cv2.putText(frame, "Motion detected", (50,50), font, 1, (0,255,0), 2)
