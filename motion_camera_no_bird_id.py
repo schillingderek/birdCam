4565907cdc0d9769bc84d2cbca50dd1de7862520
schillingderek <schilling.derek@gmail.com>
2024-07-18 12:38:56 -0500

    try stopping and starting camera when snap

--- motion_camera_no_bird_id.py
+++ motion_camera_no_bird_id.py
@@ -190,12 +190,22 @@ class Camera:
 
     async def video_snap(self):
         print("Snap")
-        timestamp = datetime.now()
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         print(timestamp)
         self.file_output = f"/home/schillingderek/SecurityCamera/static/images/snap_{timestamp}.jpg"
-        self.job = self.camera.switch_mode_and_capture_file(self.still_config, self.file_output, wait=False)
+
+        self.camera.stop_recording()  # Ensure recording is stopped before switching mode
+        self.camera.configure(self.still_config)
+        self.camera.start()  # Start the camera in still mode
+
+        self.job = self.camera.capture_file(self.file_output)
         self.metadata = self.camera.wait(self.job)
+
         await self.send_image_for_processing(self.file_output)
+
+        self.camera.stop()  # Stop the camera after capturing the image
+        self.camera.configure(self.video_config)
+        self.camera.start_recording(self.encoder, output)  # Restart recording
 
     def get_frame(self):
         self.camera.start()
