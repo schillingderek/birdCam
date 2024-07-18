36750fd7ad6b3c7e727b95df7ec67f1fb2af20ba
schillingderek <schilling.derek@gmail.com>
2024-07-18 13:28:33 -0500

    put back normal videosnap method

--- motion_camera_no_bird_id.py
+++ motion_camera_no_bird_id.py
@@ -285,12 +285,22 @@ class Camera:
 
     def VideoSnap(self):
         print("Snap")
-        timestamp = datetime.now()
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         print(timestamp)
         self.file_output = f"/home/schillingderek/SecurityCamera/static/images/snap_{timestamp}.jpg"
 
-        self.job = self.camera.switch_mode_and_capture_file(self.still_config, self.file_output, wait=False)
+        self.camera.stop_recording()  # Ensure recording is stopped before switching mode
+        self.camera.configure(self.still_config)
+        self.camera.start()  # Start the camera in still mode
+
+        self.job = self.camera.capture_file(self.file_output)
         self.metadata = self.camera.wait(self.job)
+
+        # await self.send_image_for_processing(self.file_output)
+
+        self.camera.stop()  # Stop the camera after capturing the image
+        self.camera.configure(self.video_config)
+        self.camera.start_recording(self.encoder, output)  # Restart recording
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -384,8 +394,8 @@ def bird_info():
 def snap():
     """Snap Pane"""
     print("Taking a photo")
-    # camera.VideoSnap()
-    asyncio.run(camera.video_snap())
+    camera.VideoSnap()
+    # asyncio.run(camera.video_snap())
     return render_template('snap.html')
 
 @app.route('/api/files')
