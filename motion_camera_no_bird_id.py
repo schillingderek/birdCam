2e9e486ff7dabb8c92dd831af386183a30142718
schillingderek <schilling.derek@gmail.com>
2024-07-18 11:21:44 -0500

    lowering res

--- motion_camera_no_bird_id.py
+++ motion_camera_no_bird_id.py
@@ -151,7 +151,7 @@ def send_email(subject, body, sender, receiver, password):
 class Camera:
     def __init__(self):
         self.camera = picamera2.Picamera2()
-        self.camera.configure(self.camera.create_video_configuration(main={"size": (1920, 1080)}))
+        self.camera.configure(self.camera.create_video_configuration(main={"size": (800, 600)}))
         self.still_config = self.camera.create_still_configuration()
         self.encoder = MJPEGEncoder(10000000)
         self.streamOut = StreamingOutput()
