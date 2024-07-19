f1f0a4b93151347122fd0debf9e65f781cd7eae3
schillingderek <schilling.derek@gmail.com>
2024-07-19 11:55:34 -0500

    small still image

--- motion_camera_remote_bird_id.py
+++ motion_camera_remote_bird_id.py
@@ -154,7 +154,7 @@ class Camera:
         self.camera = picamera2.Picamera2()
         # self.video_config = self.camera.create_video_configuration(main={"size": (1280, 720)})
         self.video_config = self.camera.create_video_configuration(main={"size": (1024, 600)})
-        self.still_config = self.camera.create_still_configuration()
+        self.still_config = self.camera.create_still_configuration(main={"size": (1920, 1080)})
         self.camera.configure(self.video_config)
         self.encoder = MJPEGEncoder(10000000)
         self.streamOut = StreamingOutput()
