1e7412098537175bb1b9abd389ecaa9593cead83
schillingderek <schilling.derek@gmail.com>
2024-07-19 15:41:28 -0500

    reduce image size

--- motion_camera_remote_bird_id.py
+++ motion_camera_remote_bird_id.py
@@ -294,9 +294,9 @@ class Camera:
     def capture_frame(self):
         print("Capturing image at high res")
         timestamp = datetime.now()
-        self.still_config = self.camera.create_still_configuration()
+        self.still_config = self.camera.create_still_configuration(size = (1920, 1080))
         self.file_output = f"/home/schillingderek/SecurityCamera/static/images/snap_{timestamp}.jpg"
-        self.job = self.camera.switch_mode_and_capture_file(self.still_config, self.file_output, wait=True)
+        self.job = self.camera.switch_mode_and_capture_file(self.still_config, self.file_output, wait=False)
         self.metadata = self.camera.wait(self.job)
         self.uploadFile()
 
