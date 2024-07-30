62d3a7341776fc0db0003176e93963ba7bc28b49
schillingderek <schilling.derek@gmail.com>
2024-07-30 12:51:42 -0500

    that was a bad idea

--- motion_camera_remote_bird_id.py
+++ motion_camera_remote_bird_id.py
@@ -200,8 +200,8 @@ class Camera:
         self.drive_image_id = None
 
         # Start motion detection thread
-        self.gen_frames_thread = threading.Thread(target=self.gen_frames_loop)
-        self.gen_frames_thread.start()
+        self.motion_detection_thread = threading.Thread(target=self.motion_detection_loop)
+        self.motion_detection_thread.start()
 
     def perform_obj_detection_and_inference(self):
             # logging.info("Processing frame at: ", self.file_output)
@@ -241,21 +241,15 @@ class Camera:
 
                                                                         # Motion Detection Handler
 
-    def gen_frames_loop(self):
+    def motion_detection_loop(self):
         global frame_data
         while True:
             frame_data = self.get_frame()
-            motion_thread = threading.Thread(target=self.process_image_and_detect_motion)
-            motion_thread.start() 
-
-    def process_image_and_detect_motion(self):
-        global frame_data
-
-        image = Image.open(io.BytesIO(frame_data)).convert('L')  # Convert to grayscale
-        image = image.filter(ImageFilter.GaussianBlur(radius=2))  # Apply Gaussian blur
-        if self.previous_image is not None:
-            self.detect_motion(self.previous_image, image)
-        self.previous_image = image
+            image = Image.open(io.BytesIO(frame_data)).convert('L')  # Convert to grayscale
+            image = image.filter(ImageFilter.GaussianBlur(radius=2))  # Apply Gaussian blur
+            if self.previous_image is not None:
+                self.detect_motion(self.previous_image, image)
+            self.previous_image = image
     
     def detect_motion(self, prev_image, current_image):
         global last_motion_time, current_video_file
