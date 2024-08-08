781e71a356a79e99fbc2ecf40d000da372123814
schillingderek <schilling.derek@gmail.com>
2024-08-08 08:39:15 -0500

    updating to use lores stream

--- streamingServer/stream_picamera_h264/server.py
+++ streamingServer/stream_picamera_h264/server.py
@@ -8,20 +8,25 @@ from picamera2.outputs import FileOutput, CircularOutput
 from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 from wsgiref.simple_server import make_server
 from ws4py.websocket import WebSocket
-from ws4py.server.wsgirefserver import WSGIServer, WebSocketWSGIHandler, WebSocketWSGIRequestHandler
+from ws4py.server.wsgirefserver import (
+    WSGIServer,
+    WebSocketWSGIHandler,
+    WebSocketWSGIRequestHandler,
+)
 from ws4py.server.wsgiutils import WebSocketWSGIApplication
 from threading import Thread, Condition
 import time
 from PIL import Image
 from datetime import datetime
 
-videoCaptureEncoder = H264Encoder()
-videoCaptureOutput = CircularOutput()
+video_capture_endoder = H264Encoder()
+video_capture_output = CircularOutput()
 
 startTime = time.time()
 
 width = 960
 height = 540
+
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -37,40 +42,47 @@ class StreamingOutput(io.BufferedIOBase):
         with self.condition:
             self.condition.wait()
             return self.frame
-        
+
+
 class Camera:
     def __init__(self):
-        self.camera = picamera2.Picamera2()
-        self.video_config = self.camera.create_video_configuration({"size": (width, height)}, lores={"size": (640, 360)})
-        self.camera.configure(self.video_config)
+        self.picamera = picamera2.Picamera2()
+        self.video_config = self.picamera.create_video_configuration(
+            {"size": (width, height)}, lores={"size": (640, 360)}
+        )
+        self.picamera.configure(self.video_config)
         self.streaming_encoder = H264Encoder()
         self.streaming_encoder.bitrate = 2500000
-        self.streaming_encoder.profile = 'baseline'
-        self.streaming_encoder.size = self.video_config['lores']['size']
-        self.streaming_encoder.format = self.video_config['lores']['format']
+        self.streaming_encoder.profile = "baseline"
         self.stream_out = StreamingOutput()
         self.stream_out_2 = FileOutput(self.stream_out)
-        self.streaming_encoder.output = [self.stream_out_2]
-        self.camera.start_encoder(self.streaming_encoder)
-        
-        self.camera.start_recording(videoCaptureEncoder, videoCaptureOutput)
+        self.streaming_output = [self.stream_out_2]
 
-        self.camera.start()
+        self.picamera.start()
+
 
 camera = Camera()
 
+
 def stream():
-    global startTime
-    is_recording = False
+
+    camera.picamera.start_encoder(camera.streaming_encoder, camera.streaming_output, name='lores')
+    camera.picamera.start_encoder(video_capture_endoder, video_capture_output, name='main')
+    # global startTime
+    # is_recording = False
     try:
-        WebSocketWSGIHandler.http_version = '1.1'
-        websocketd = make_server('', 9000, server_class=WSGIServer,
-                 handler_class=WebSocketWSGIRequestHandler,
-                 app=WebSocketWSGIApplication(handler_cls=WebSocket))
+        WebSocketWSGIHandler.http_version = "1.1"
+        websocketd = make_server(
+            "",
+            9000,
+            server_class=WSGIServer,
+            handler_class=WebSocketWSGIRequestHandler,
+            app=WebSocketWSGIApplication(handler_cls=WebSocket),
+        )
         websocketd.initialize_websockets_manager()
         websocketd_thread = Thread(target=websocketd.serve_forever)
 
-        httpd = ThreadingHTTPServer(('', 8000), SimpleHTTPRequestHandler)
+        httpd = ThreadingHTTPServer(("", 8000), SimpleHTTPRequestHandler)
         httpd_thread = Thread(target=httpd.serve_forever)
 
         try:
@@ -81,7 +93,7 @@ def stream():
                 # Read from the StreamingOutput and broadcast via WebSocket
                 frame_data = camera.stream_out.read()
                 if frame_data:
-                    #print("Sending frame of size:", len(frame_data))
+                    # print("Sending frame of size:", len(frame_data))
                     websocketd.manager.broadcast(frame_data, binary=True)
                 else:
                     print("No frame data received")
@@ -103,15 +115,15 @@ def stream():
         finally:
             websocketd.shutdown()
             httpd.shutdown()
-            camera.camera.stop()
-            camera.camera.stop_encoder()
+            camera.picamera.stop()
+            camera.picamera.stop_encoder()
             raise KeyboardInterrupt
     except KeyboardInterrupt:
         pass
     finally:
-        camera.camera.stop()
-        camera.camera.stop_encoder()
+        camera.picamera.stop()
+        camera.picamera.stop_encoder()
+
 
 if __name__ == "__main__":
     stream()
-
